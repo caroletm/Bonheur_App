848bbe8a0e892644da853a25ea3b6912cc949832
Carole Trem <trem.carole@gmail.com>
2025-09-11 10:13:46 +0200

    first commit

---
+++ Bonheur_App/Bonheur_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bonheur_AppApp.swift
+//  Bonheur_App
+//
+//  Created by caroletm on 11/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Bonheur_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
