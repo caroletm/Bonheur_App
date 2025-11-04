2c5a25d89010f2ebb648efc13c0d91ed66c7d67f
swisslson <cyril.hany@gmail.com>
2025-11-04 10:08:31 +0100

    ajout visite

--- Bonheur_App/Features/Global/Data/API/VisiteService.swift
+++ Bonheur_App/Features/Global/Data/API/VisiteService.swift
@@ -5,3 +5,16 @@
 //  Created by caroletm on 29/10/2025.
 //
 
+import Foundation
+
+final class VisiteService {
+    static let shared = VisiteService()
+        private init() {}
+    func createVisite(_ dto: VisiteCreateDTO) async throws -> VisiteDTO {
+            guard let token = UserDefaults.standard.string(forKey: "authToken") else {
+                throw URLError(.userAuthenticationRequired)
+            }
+            return try await APIService.shared.post("/visites", body: dto, token: token)
+        }
+}
+
